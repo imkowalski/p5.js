0645465b3047aaddc60acf96af905cd63e609659
Michal Kowalski <michalkowal05@gmail.com>
2021-12-07 11:29:04 +0100

    done

--- kalenderlys/sketch.js
+++ kalenderlys/sketch.js
@@ -1,22 +1,27 @@
 
 const screen_size = 600;
 const height = 400;
-const width = 80;
-let pos, fire, candle, numb, table;
+const width = 65;
+let pos, fire, candle, numb, table, wall, tree, outside;
 let fireI = [];
 let snowflakes = [];
 let auto = true;
 let is_december = true;
-
+let christmas_day = false;
 
 function preload() {
+  outside = loadImage("./assets/outside.png")
+  tree = loadImage("./assets/tree.png")
+  wall = loadImage("./assets/background.png")
   table = loadImage("./assets/table.png");
   fireI[0] = loadImage("./fire/1.png");
   fireI[1] = loadImage("./fire/2.png");
+  fireI[2] = loadImage("./fire/3.png");
 }
 
+
 function calculate_left() {
-  return ((day()-1)*24+hour()+1)/(24*24);
+  return ((day()-1)*24+hour()-4)/(24*24);
 }
 
 
@@ -27,8 +32,8 @@ function setup() {
   if (month() != 12) {
     is_december = false;
   } else {
-    for (let i = 0; i < 100; i++) {
-      snowflakes.push(new snowflake()); // append snowflake object
+    for (let i = 0; i < 300; i++) {
+      snowflakes.push(new snowflake()); 
     }
   
   }
@@ -36,7 +41,7 @@ function setup() {
 }
 
 
-// this function takes the value of the slider and calculates 
+// this function takes the value of the slider and calculates  it to prx and burns the candle if its set to manual
 function dom() {
   document.getElementById("val").innerHTML =  String(auto) +" | "+ document.getElementById("slider").value/1000;
   candle.burn(document.getElementById("slider").value/1000);
@@ -67,14 +72,15 @@ function S_not_december() {
 
 function S_candle_scene() {
   background("#001A45");
-  
-  // loop through snowflakes with a for..of loop
+
+  image(outside,screen_size/2,screen_size/2,screen_size,screen_size)
   for (let flake of snowflakes) {
-    flake.update(); // update snowflake position
-    flake.display(); // draw snowflake
+    flake.update(); 
+    flake.display();
   }
-  image(table,300,600,400,100)
-  
+  image(wall,screen_size/2,screen_size/2,screen_size,screen_size)
+  image(table,300,600,550,150)
+  image(tree,150,450,200,200)
   candle.draw1();
   numb.draw();
   candle.draw2();
@@ -98,12 +104,14 @@ class candleC {
     this.candle_h = height;
     this.pos = pos;
     this.og_y = 200;
-    fire = new animation(this.pos.x,screen_size-this.height,fireI);
+    fire = new animation(this.pos.x,screen_size-this.height,fireI,9);
   
   }
   burn = (prc) => {
-    if (prc > 0.97) {
-      print("It's Christmas")
+    if (prc > 0.93) {
+      fill(20)
+      textSize(80)
+      text("It's December!!",screen_size/2,screen_size/2-100)
     }
     if (this.pos.y < screen_size-30) {
       this.pos.y = this.og_y+this.height*prc;
@@ -125,7 +133,7 @@ class candleC {
   draw2 = () => {
     fill("#604C32");
     rect(this.pos.x-7.5,this.pos.y-40,15,40);
-    fire.move(this.pos.x,this.pos.y-50)
+    fire.move(this.pos.x,this.pos.y-60)
     fire.update();
   }
 }
@@ -161,8 +169,7 @@ class numbers {
 
 
 
-class snowflake {
-  // initialize coordinates
+class snowflake { 
   constructor() {
     this.posX = random(0,screen_size+200);
     this.posY = random(0,screen_size);
@@ -171,13 +178,10 @@ class snowflake {
   }
   update = () => {
     fill(255,255,255,random(200,250))
-    // x position follows a circle
-    this.posX += random(-2,1)
+    this.posX += random(-2.00,0.50)
 
-    // different size snowflakes fall at slightly different y speeds
-    this.posY += random(0.8,1.1)
+    this.posY += random(3,4)
 
-    // delete snowflake if past end of screen
     if (this.posY > screen_size) {
       this.posY = 0
       this.posX = random(0,screen_size+200);
